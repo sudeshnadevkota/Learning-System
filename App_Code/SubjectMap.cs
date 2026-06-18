c1feefa84f100c66ae9364472d81d7729b319ffc
Dorji <sherpadorjee2580@gmail.com>
2026-06-18 15:30:22 +0545

    Solved the error in the filter

---
+++ App_Code/SubjectMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Learning_System
+{
+    public static class SubjectMap
+    {
+        public class SubjectInfo
+        {
+            public string Table { get; set; }
+            public string Title { get; set; }
+        }
+
+        public static readonly Dictionary<string, SubjectInfo> Subjects =
+            new Dictionary<string, SubjectInfo>
+            {
+                { "BE", new SubjectInfo { Table = "bit_1_BE", Title = "Business English" } },
+                // Add more subjects below, same pattern:
+                // { "DSA", new SubjectInfo { Table = "bit_1_DSA", Title = "Data Structures and Algorithms" } },
+            };
+    }
+}
--- Bit_Admin/business_english.aspx.designer.cs
+++ Bit_Admin/business_english.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace Learning_System.Bit_Admin
         protected global::System.Web.UI.WebControls.DropDownList ddlFileType;
 
         /// <summary>
-        /// FileUpload1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
-
-        /// <summary>
         /// ddlContentType control.
         /// </summary>
         /// <remarks>
